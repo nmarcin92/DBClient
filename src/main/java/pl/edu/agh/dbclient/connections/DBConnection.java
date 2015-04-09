cf032ad9a8b9f38df5b9b36794f3ff8eb7e7a1bd
Marcin Nowak <nmarcin92@gmail.com>
2015-04-09 22:25:56 +0200

    #3 logger change and connection interface update

--- src/main/java/pl/edu/agh/dbclient/connections/DBConnection.java
+++ src/main/java/pl/edu/agh/dbclient/connections/DBConnection.java
@@ -1,9 +1,10 @@
 package pl.edu.agh.dbclient.connections;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.log4j.Logger;
 import pl.edu.agh.dbclient.operations.CreateOperation;
+import pl.edu.agh.dbclient.operations.DeleteOperation;
 import pl.edu.agh.dbclient.operations.ReadOperation;
+import pl.edu.agh.dbclient.operations.UpdateOperation;
 import pl.edu.agh.dbclient.results.QueryResult;
 
 /**
@@ -11,12 +12,18 @@ import pl.edu.agh.dbclient.results.QueryResult;
  */
 public interface DBConnection {
 
-    static final Logger LOGGER = LoggerFactory.getLogger(DBConnection.class);
+    static final Logger LOGGER = Logger.getLogger(DBConnection.class);
 
     public void setCredentials(DBCredentials credentials);
 
     public QueryResult performCreate(CreateOperation operation);
 
     public QueryResult performRead(ReadOperation operation);
+
+    public QueryResult performUpdate(UpdateOperation operation);
+
+    public QueryResult performDelete(DeleteOperation operation);
+
+    public QueryResult executeCommand(String command);
 
 }
--- src/main/java/pl/edu/agh/dbclient/utils/Configuration.java
+++ src/main/java/pl/edu/agh/dbclient/utils/Configuration.java
@@ -1,12 +1,8 @@
 package pl.edu.agh.dbclient.utils;
 
-import com.google.common.io.Resources;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.log4j.Logger;
 
-import java.net.URL;
 import java.util.MissingResourceException;
-import java.util.Properties;
 import java.util.ResourceBundle;
 
 /**
@@ -14,7 +10,7 @@ import java.util.ResourceBundle;
  */
 public class Configuration {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(Configuration.class);
+    private static final Logger LOGGER = Logger.getLogger(Configuration.class);
     private static final String PROP_FILENAME = "config";
     private static final ResourceBundle BUNDLE;
 
@@ -46,6 +42,8 @@ public class Configuration {
         }
     }
 
-
+    public static Integer getInteger(String key) {
+        return getInteger(key, null);
+    }
 
 }
