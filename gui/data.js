98758fff2f690cc4d1e0a3205c3295637697e60e
Iwan Bugajski <iwan.bugajski@gmail.com>
2015-04-24 01:52:13 +0200

    gui - initial commit

---
+++ gui/data.js
@@ -0,0 +1,94 @@
+var json = {
+    "success": true,
+    "entity": {
+        "entityName": "tabela_2",
+        "attributes": [
+            {
+                "attributeName": "aaa",
+                "dataType": "character"
+            },
+            {
+                "attributeName": "tes",
+                "dataType": "character varying"
+            },
+            {
+                "attributeName": "ddd",
+                "dataType": "integer"
+            }
+        ],
+        "rows": [
+            {
+                "attributes": {
+                    "aaa": "1",
+                    "tes": null,
+                    "ddd": null
+                }
+            },
+            {
+                "attributes": {
+                    "aaa": "2",
+                    "tes": null,
+                    "ddd": null
+                }
+            },
+            {
+                "attributes": {
+                    "aaa": "2",
+                    "tes": "aa",
+                    "ddd": null
+                }
+            },
+            {
+                "attributes": {
+                    "aaa": null,
+                    "tes": "aa",
+                    "ddd": null
+                }
+            },
+            {
+                "attributes": {
+                    "aaa": null,
+                    "tes": "aa",
+                    "ddd": null
+                }
+            },
+            {
+                "attributes": {
+                    "aaa": "a",
+                    "tes": null,
+                    "ddd": null
+                }
+            },
+            {
+                "attributes": {
+                    "aaa": "a",
+                    "tes": "aaa",
+                    "ddd": "55"
+                }
+            },
+            {
+                "attributes": {
+                    "aaa": "e",
+                    "tes": "elo520",
+                    "ddd": "1"
+                }
+            },
+            {
+                "attributes": {
+                    "aaa": "e",
+                    "tes": "elo520",
+                    "ddd": "1"
+                }
+            },
+            {
+                "attributes": {
+                    "aaa": "e",
+                    "tes": "elo520",
+                    "ddd": "1"
+                }
+            }
+        ]
+    },
+    "errors": [],
+    "nextPage": false
+};
