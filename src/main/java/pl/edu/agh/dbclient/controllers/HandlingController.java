58c27077753ebe9e3ba0a56e06bc13baebdbe46d
Marcin Nowak <nmarcin92@gmail.com>
2015-05-14 21:38:18 +0200

    errors fix

--- src/main/java/pl/edu/agh/dbclient/controllers/HandlingController.java
+++ src/main/java/pl/edu/agh/dbclient/controllers/HandlingController.java
@@ -1,5 +1,6 @@
 package pl.edu.agh.dbclient.controllers;
 
+import org.apache.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,15 +13,22 @@ import pl.edu.agh.dbclient.objects.QueryResult;
  * @author mnowak
  */
 public class HandlingController {
+    private static final Logger LOGGER = Logger.getLogger(HandlingController.class);
 
     @ExceptionHandler(DBClientException.class)
-    public @ResponseBody QueryResult handleError(DBClientException exception) {
+    public
+    @ResponseBody
+    QueryResult handleError(DBClientException exception) {
+        LOGGER.error("Unknown error", exception);
         return QueryResult.createErrorResult(exception.getMessage());
     }
 
     @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(Throwable.class)
-    public @ResponseBody QueryResult handleError() {
+    public
+    @ResponseBody
+    QueryResult handleError(Throwable t) {
+        LOGGER.error("Unexpected exception", t);
         return QueryResult.createErrorResult(WebAppConstants.UNKNOWN_SERVER_ERROR);
     }
 
